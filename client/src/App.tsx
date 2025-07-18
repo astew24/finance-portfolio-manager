5b09b78ccbaf1b3e3c5f8e7a97000961119d55dd
astew24e <andrewduncan1012@gmail.com>
2025-07-18 17:02:37 -0400

    Initial commit: project setup, backend, and frontend base

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="dashboard">
+      <h1>Finance Portfolio Manager</h1>
+      <p>Welcome! Your portfolio will appear here.</p>
+      {/* You can add more features here later */}
+    </div>
+  )
+}
+
+export default App
